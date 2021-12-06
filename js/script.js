96d1b9e0c8acc2b4e2a45280c721c01147b7e417
aigerim2002 <aigerim.ashyrova@gmail.com>
2021-12-06 16:29:20 +0600

    initial

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(function () {
+
+    $('.star').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+        prevArrow:' <div class="slider"><img  class="slider__icon" src="./img/Icon.svg" alt=""></div>',
+        nextArrow:'<div class="slider"<img  class="slider__icon" src="./img/Icon.svg" alt=""></div>'
+});
+});
